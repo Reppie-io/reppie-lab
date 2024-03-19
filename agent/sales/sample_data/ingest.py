cad8b51636d69f9de5089aa68be226184eda39f1
Caio Bonfante <caio.bonfante@gmail.com>
2024-03-19 18:31:02 -0300

    orchestration chain poc

--- agent/sales/sample_data/ingest.py
+++ agent/sales/sample_data/ingest.py
@@ -7,14 +7,15 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv((find_dotenv()))
 
-
-loader = TextLoader("sample_product_catalog.txt")
+# loader = TextLoader("sample_product_catalog.txt")
+loader = TextLoader("sample_qualification_criteria.txt")
 documents = loader.load()
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 docs = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
 
-index_name = "reppie-labs-sales-agent"
+# index_name = "reppie-labs-sales-agent"
+index_name = "reppie-labs-sdr-agent-qualification-criteria"
 PineconeVectorStore.from_documents(docs, embeddings, index_name=index_name)
